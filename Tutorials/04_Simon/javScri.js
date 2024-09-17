ce00da2dd88be79d47a660543930f8d905bb758a
Jose Pablo Soto Sanchez <0262205@up.edu.mx>
2024-09-16 20:10:02 -0600

    Added changing when clicked

--- Tutorials/04_Simon/javScri.js
+++ Tutorials/04_Simon/javScri.js
@@ -1,17 +1,32 @@
-const buttonColors = ["red", "blue", "green", "yellow"];
-let gamePattern = [];
+var buttonColors = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+var  userClickedPattern = [];
 
 function nextSequence() {
-    const randomNumber = Math.floor(Math.random() * 4);
-    const randomChosenColor = buttonColors[randomNumber];
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColor = buttonColors[randomNumber];
     gamePattern.push(randomChosenColor);
     console.log(gamePattern);
-
     $("#" + randomChosenColor).fadeOut(100).fadeIn(100);
     playSound(randomChosenColor);
 }
 
 function playSound(name) {
-    const audio = new Audio("sounds/" + name + ".mp3");
+    var audio = new Audio("sounds/" + name + ".mp3");
     audio.play();
 }
+
+function animatePress(currentColor) {
+    $("#" + currentColor).addClass("pressed");
+    setTimeout(function() {
+        $("#" + currentColor).removeClass("pressed");
+    }, 100);
+}
+
+$(".btn").click(function() {
+    var userChosenColor = $(this).attr("id");
+    userClickedPattern.push(userChosenColor);
+    console.log(userClickedPattern);
+    playSound(userChosenColor);
+    animatePress(userChosenColor);
+});
