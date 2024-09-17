83ff80f4322601c9cf3a6083efaab116853bbf2f
Jose Pablo Soto Sanchez <0262205@up.edu.mx>
2024-09-16 19:17:40 -0600

    Updated the game

--- Tutorials/04_Simon/javScri.js
+++ Tutorials/04_Simon/javScri.js
@@ -1,15 +1,23 @@
+// Variables globales
 const buttonColors = ["red", "blue", "green", "yellow"];
-let patterns = [];
+let gamePattern = [];
 
+// Función nextSequence
+function nextSequence() {
+    // Genera un número aleatorio entre 0 y 3
+    const randomNumber = Math.floor(Math.random() * 4);
+    
+    // Almacena el color correspondiente al número generado
+    const randomChosenColor = buttonColors[randomNumber];
+    
+    // Inserta el color en el patrón del juego
+    gamePattern.push(randomChosenColor);
 
+    // Verificar en la consola
+    console.log(gamePattern);
 
-function nextSequence(){
-    const guardar= Math.floor(Math.random() * 4);
-    patterns.push(guardar);
-    return guardar;
+    // Selecciona el botón usando jQuery y añade un efecto de parpadeo
+    $("#" + randomChosenColor).fadeOut(100).fadeIn(100);
 }
 
-console.log(nextSequence())
-console.log(patterns)
-
-
+$("#" + randomChosenColor).fadeOut(100).fadeIn(100);
