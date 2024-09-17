bc5d6c59e7842febacec418844ae9f40763a8e49
Jose Pablo Soto Sanchez <0262205@up.edu.mx>
2024-09-16 19:30:03 -0600

    Sound audio added

--- Tutorials/04_Simon/javScri.js
+++ Tutorials/04_Simon/javScri.js
@@ -1,23 +1,17 @@
-// Variables globales
 const buttonColors = ["red", "blue", "green", "yellow"];
 let gamePattern = [];
 
-// Función nextSequence
 function nextSequence() {
-    // Genera un número aleatorio entre 0 y 3
     const randomNumber = Math.floor(Math.random() * 4);
-    
-    // Almacena el color correspondiente al número generado
     const randomChosenColor = buttonColors[randomNumber];
-    
-    // Inserta el color en el patrón del juego
     gamePattern.push(randomChosenColor);
-
-    // Verificar en la consola
     console.log(gamePattern);
 
-    // Selecciona el botón usando jQuery y añade un efecto de parpadeo
     $("#" + randomChosenColor).fadeOut(100).fadeIn(100);
+    playSound(randomChosenColor);
 }
 
-$("#" + randomChosenColor).fadeOut(100).fadeIn(100);
+function playSound(name) {
+    const audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
