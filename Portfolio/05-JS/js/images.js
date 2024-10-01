44cab798e83cf727ad3dba3dbe74fbf137ca94d0
Jose Pablo Soto Sanchez <0262205@up.edu.mx>
2024-10-01 17:42:08 -0600

    Finished piglatin in 05

--- Portfolio/05-JS/js/images.js
+++ Portfolio/05-JS/js/images.js
@@ -62,10 +62,10 @@ function component(width, height, color, x, y, type) {
     this.y += this.speedY;
     if (this.type == "image") {
       if (this.x >= canvasWidth - this.width / 2 || this.x <= 0) {
-        //TODO: make the image bounce back when reaching the edges
+        myGamePiece.speedX *= -1;
       }
       if (this.y >= canvasHeight - this.height / 2 || this.y <= 0) {
-        //TODO: make the image bounce back when reaching the edges
+        myGamePiece.speedY *= -1;
       }
     }
   };
@@ -84,13 +84,14 @@ function moveup() {
 }
 
 function movedown() {
-  //TODO: implement the move down functionality
+  myGamePiece.speedY += 1;
 }
 
 function moveleft() {
-  //TODO: implement the move left functionality
+  myGamePiece.speedX -= 1;
 }
 
 function moveright() {
   myGamePiece.speedX += 1;
 }
+
--- Portfolio/05-JS/js/pig_latin.js
+++ Portfolio/05-JS/js/pig_latin.js
@@ -3,31 +3,46 @@ Pig Latin
 */
 
 function igpayAtinlay(str) {
-  // TODO: Initialize the word array properly
-  var returnArray = [],
-    wordArray = [];
-  // TODO: make sure that the output is being properly built to produce the desired result.
+  var returnArray = [];
+  var wordArray = str.split(" "); // Split the input string into words
+
   for (var i = 0; i < wordArray.length; i++) {
     var word = wordArray[i];
-    var beginning = word.charAt(0);
+    var beginning = "";
+    var restOfWord = "";
+    var vowelFound = false;
 
-    if (/[aeiouAEIOU]/.test(beginning)) {
-      returnArray.push(word);
-      continue;
-    }
-
-    for (var ii = 1; ii < word.length; ii++) {
-      if (/[aeiouAEIOU]/.test(word.charAt(ii))) {
-        break;
+    if (/[aeiouAEIOU]/.test(word.charAt(0))) {
+      returnArray.push(word + "way");
+    } else {
+      for (var ii = 0; ii < word.length; ii++) {
+        if (/[aeiouAEIOU]/.test(word.charAt(ii))) {
+          vowelFound = true;
+          restOfWord = word.slice(ii);
+          break;
+        } else {
+          beginning += word.charAt(ii);
+        }
+      }
+      if (vowelFound) {
+        returnArray.push(restOfWord + beginning + "ay");
       } else {
-        beginning += word.charAt(ii);
+        returnArray.push(word + "ay");
       }
     }
   }
+  document.getElementById("pigLatLbl").textContent=(returnArray.join(" "));
   return returnArray.join(" ");
 }
 
-// Some examples of expected outputs
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById("btn").addEventListener("click", function() {
+    var text = document.getElementById("txtVal").value;
+    igpayAtinlay(text); 
+  });
+});
+
+// Test examples
 console.log(igpayAtinlay("pizza")); // "izzapay"
 console.log(igpayAtinlay("apple")); // "appleway"
 console.log(igpayAtinlay("happy meal")); // "appyhay ealmay"
