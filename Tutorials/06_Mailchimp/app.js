9f50796063fa3f999a26aa52382915aab2aaeb6d
Jose Pablo Soto Sanchez <0262205@up.edu.mx>
2024-11-17 14:00:07 -0600

    dotenv

--- Tutorials/06_Mailchimp/app.js
+++ Tutorials/06_Mailchimp/app.js
@@ -1,6 +1,8 @@
 const { name } = require("ejs");
 const express = require("express");
 const app = express();
+require("dotenv").config();
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
